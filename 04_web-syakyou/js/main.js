db1fd88f20e91c18ff5f8d94d6d00e4532b6fe34
SoumaKaori <soudog321@gmail.com>
2020-03-10 21:22:04 +0900

    syakyou no.2

---
+++ 04_web-syakyou/js/main.js
@@ -0,0 +1,72 @@
+$(function(){
+  $(window).scroll(function(){
+    $('.fadein').each(function(){
+      var targetElement = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if(scroll > targetElement - windowHeight + 200){
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    speed:100,
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: true
+   },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+  });
+
+});
+
+var pagetop = $('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 600);
+     return false;
+  });
+$(function() {
+    $('.policy').on('click',function(){
+      $('.js-modal').fadeIn();
+      return false;
+      var posi = $(window).scrollTop();
+      $('[data-modal="fixed"]').css({
+        position: 'fixed',
+        top: -1 * posi
+      });
+    });
+    $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      return false;
+    });
+
+  });
